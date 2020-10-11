0314f0622fba5b2efc083e10e8098c4e0c18e2e0
Rexsunon <osikhenaevidence@gmail.com>
2020-10-11 17:09:15 +0100

    Initial commit

---
+++ lib/widgets/vertical_icon_button.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class VerticalIconButton extends StatelessWidget {
+  final IconData icon;
+  final String title;
+  final Function onTap;
+
+  const VerticalIconButton({Key key, this.icon, this.title, this.onTap})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Column(
+        children: [
+          Icon(icon),
+          Text(title),
+        ],
+      ),
+    );
+  }
+}
